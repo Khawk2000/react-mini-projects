c39bcb9c823e5ab7c40c7c757a973c654548839d
keeganmhawkins@gmail.com <keeganmhawkins@gmail.com>
2024-08-06 13:14:06 -0500

    Completed remake of expense tracker

---
+++ expense-tracker-nextjs/README.md
@@ -0,0 +1,3 @@
+# Remake of Expense Tracker
+
+### Practice using more NextJS, TypeScript, and exposure to Clerk, Neon database, and Prisma
---
+++ expense-tracker-nextjs/app/page.tsx
@@ -0,0 +1,26 @@
+import AddTransaction from "@/components/AddTransaction";
+import Balance from "@/components/Balance";
+import Guest from "@/components/Guest";
+import IncomeExpense from "@/components/IncomeExpense";
+import TransactionList from "@/components/TransactionList";
+import { currentUser } from "@clerk/nextjs/server";
+
+const HomePage = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return <Guest />;
+  }
+
+  return (
+    <main>
+      <h2>Welcome, {user.firstName}</h2>
+      <Balance />
+      <IncomeExpense />
+      <AddTransaction />
+      <TransactionList />
+    </main>
+  );
+};
+
+export default HomePage;
